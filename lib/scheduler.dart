0c7e8d2d6e652a49fe47495f0585cad53fc5b1e3
Brad Proctor <bradster95@me.com>
2019-11-18 10:49:26 -0800

    Updated Scheduler

--- lib/scheduler.dart
+++ lib/scheduler.dart
@@ -17,7 +17,7 @@ class _ScheduleState extends State<Schedule> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Scheduler"),
+        title: Text("Scheduler-Burbank"),
         centerTitle: true,
       ),
       body: Center(
@@ -27,15 +27,44 @@ class _ScheduleState extends State<Schedule> {
             Expanded(
               child: ListView(
                 children: <Widget>[
-                  Text("Summer"),
-                  Card(
-                    child: Text('12am - 7am'),
+                  Text("Winter",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      color: Colors.blue,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Container(
+                            height: 133.33,
+                            child: const ListTile(
+                              title: Text('12am - 8am',
+                                  style: TextStyle(fontSize: 24)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  Card(
-                    child: Text('7am - 7pm'),
-                  ),
-                  Card(
-                    child: Text('7pm - 12am'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      color: Colors.green,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Container(
+                            height: 266.67,
+                            child: const ListTile(
+                              title: Text('8am - 11pm',
+                                  style: TextStyle(fontSize: 24)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
@@ -43,15 +72,80 @@ class _ScheduleState extends State<Schedule> {
             Expanded(
               child: ListView(
                 children: <Widget>[
-                  Text("Winter"),
-                  Card(
-                    child: Text('12am - 7am'),
+                  Text("Summer",
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      color: Colors.yellow,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Container(
+                            height: 133.33,
+                            child: const ListTile(
+                              title: Text('12am - 8am',
+                                  style: TextStyle(fontSize: 24)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  Card(
-                    child: Text('7am - 7pm'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      color: Colors.orange,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Container(
+                            height: 133.33,
+                            child: const ListTile(
+                              title: Text('8am - 4pm',
+                                  style: TextStyle(fontSize: 24)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  Card(
-                    child: Text('7pm - 12am'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.8),
+                    child: Card(
+                      color: Colors.red,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Container(
+                            height: 50,
+                            child: const ListTile(
+                              title: Text('4pm - 7pm',
+                                  style: TextStyle(fontSize: 24)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      color: Colors.orange,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Container(
+                            height: 66.67,
+                            child: const ListTile(
+                              title: Text('7pm - 12am',
+                                  style: TextStyle(fontSize: 24)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ],
               ),
