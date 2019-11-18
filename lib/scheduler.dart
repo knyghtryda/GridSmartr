d22f714b80efb085d94694ca818c2b6920267a53
Brad Proctor <bradster95@me.com>
2019-11-18 11:58:44 -0800

    Updated Scheduler - Final

--- lib/scheduler.dart
+++ lib/scheduler.dart
@@ -16,8 +16,11 @@ class _ScheduleState extends State<Schedule> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffa9a9a9),
       appBar: AppBar(
-        title: Text("Scheduler-Burbank"),
+        title: Text(
+          "Scheduler-Burbank",
+        ),
         centerTitle: true,
       ),
       body: Center(
@@ -25,129 +28,140 @@ class _ScheduleState extends State<Schedule> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             Expanded(
-              child: ListView(
-                children: <Widget>[
-                  Text("Winter",
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      color: Colors.blue,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Container(
-                            height: 133.33,
-                            child: const ListTile(
-                              title: Text('12am - 8am',
-                                  style: TextStyle(fontSize: 24)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView(
+                  children: <Widget>[
+                    Center(
+                      child: Text("Winter",
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold)),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        color: Colors.blue,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(
+                              height: 133.33,
+                              child: const ListTile(
+                                title: Text('12am - 8am',
+                                    style: TextStyle(fontSize: 24)),
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      color: Colors.green,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Container(
-                            height: 266.67,
-                            child: const ListTile(
-                              title: Text('8am - 11pm',
-                                  style: TextStyle(fontSize: 24)),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        color: Colors.green,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(
+                              height: 266.67,
+                              child: const ListTile(
+                                title: Text('8am - 11pm',
+                                    style: TextStyle(fontSize: 24)),
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             Expanded(
-              child: ListView(
-                children: <Widget>[
-                  Text("Summer",
-                      style:
-                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      color: Colors.yellow,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Container(
-                            height: 133.33,
-                            child: const ListTile(
-                              title: Text('12am - 8am',
-                                  style: TextStyle(fontSize: 24)),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView(
+                  children: <Widget>[
+                    Center(
+                      child: Text("Summer",
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold)),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        color: Colors.yellow,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(
+                              height: 133.33,
+                              child: const ListTile(
+                                title: Text('12am - 8am',
+                                    style: TextStyle(fontSize: 24)),
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      color: Colors.orange,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Container(
-                            height: 133.33,
-                            child: const ListTile(
-                              title: Text('8am - 4pm',
-                                  style: TextStyle(fontSize: 24)),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        color: Colors.orange,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(
+                              height: 133.33,
+                              child: const ListTile(
+                                title: Text('8am - 4pm',
+                                    style: TextStyle(fontSize: 24)),
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      color: Colors.red,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Container(
-                            height: 50,
-                            child: const ListTile(
-                              title: Text('4pm - 7pm',
-                                  style: TextStyle(fontSize: 24)),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                      child: Card(
+                        color: Colors.red,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(
+                              height: 50,
+                              child: const ListTile(
+                                title: Text('4pm - 7pm',
+                                    style: TextStyle(fontSize: 24)),
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Card(
-                      color: Colors.orange,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Container(
-                            height: 66.67,
-                            child: const ListTile(
-                              title: Text('7pm - 12am',
-                                  style: TextStyle(fontSize: 24)),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                      child: Card(
+                        color: Colors.orange,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Container(
+                              height: 66.67,
+                              child: const ListTile(
+                                title: Text('7pm - 12am',
+                                    style: TextStyle(fontSize: 24)),
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ],
